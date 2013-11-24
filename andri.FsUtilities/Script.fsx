30880b67454c795c2ead0311e1df58f7b476368f
andri <andriniaina@gmail.com>
2013-11-24 17:43:32 +0100

    anciens fichiers oublies

---
+++ andri.FsUtilities/Script.fsx
@@ -0,0 +1,29 @@
+﻿
+#load "XML.fs"
+#load "XPath.fs"
+open andri.FsUtilities
+
+open System.IO
+let sxml = @"<contacts>
+  <contact>
+    <name att='s'>Patrick Hines</name>
+    <phone>206-555-0144</phone>
+    <address>
+      <street1>123 Main St</street1>
+      <city>Mercer Island</city>
+      <state>WA</state>
+      <postal>68042</postal>
+    </address>
+  </contact>
+</contacts>"
+
+
+let doc = fXML.loadString sxml
+doc
+    |> fXML.createNavidator
+    |> fXPath.selectNodes "contacts/contact/name/@att"
+    |> Seq.head
+        |> fXPath.setValue "Andri atertsdfgsd"
+
+
+doc.InnerXml
---
+++ andri.FsUtilities/Strings.fs
@@ -0,0 +1,15 @@
+﻿namespace andri.Utilities
+
+
+module Strings =
+    open System
+    let (|StartsWith|) prefix (s:string) =
+        s.StartsWith(prefix)
+
+    let Substring start ``end`` (s:string) =
+        if ``end``<0 then s.Substring(start, s.Length+``end``-start) else s.Substring(start, ``end``-start)
+    let Trim (s:string) =
+        s.Trim()
+    let Compact (s:string) =
+        let spaces = System.Text.RegularExpressions.Regex(@"\s+")
+        spaces.Replace(spaces.Replace(Trim s, ""), "")
