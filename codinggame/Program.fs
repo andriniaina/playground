38b4703c66b2bb679f3703675f17176145500d1f
Andri Rakotomalala <andriniaina@gmail.com>
2019-04-22 18:06:27 +0200

    winamax fail

--- codinggame/Program.fs
+++ codinggame/Program.fs
@@ -2,11 +2,26 @@
 (* the standard input according to the problem statement. *)
 open System
 
+open Winamax
 
-let result1 = Enigma.ceasarReverse 4 "EFG"
-printfn "%s" result1
-let x = Enigma.rotor "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "BDFHJLCPRTXVZNYEIWGAKMUSQO" "EFG"
-let chars = Enigma.decode 9 ("BDFHJLCPRTXVZNYEIWGAKMUSQO","AJDKSIRUXBLHWTMCQGZNPYFVOE","EKMFLGDQVZNTOWYHXUSPAIBRCJ") "PQSACVVTOISXFXCIAMQEM"
-let result = chars |> Array.ofSeq |> String
-printfn "%s" result
+
+let w,h,map = 3,3,[Ball(2);Grass;Water;Water;Grass;Hole(false);Grass;Hole(false);Ball(1)]
+
+(*
+
+let d = findPossibleLandingSpot 0 (3,3,map) |> List.ofSeq
+assert (d.Length=2)
+let newMaps = shootEverywhere 0 2 (w,h,map) |> List.ofSeq
+
+for map in newMaps do
+    stdout.WriteLine (dump (w,h,map))
+*)
+
+let solutions = findSolutions (w,h,map) |> List.ofSeq
+for map in solutions do
+    let isAllHolesFilled = true //map |> List.filter (isHole) |> List.forall (isFilledHole)
+    if isAllHolesFilled then
+        stdout.WriteLine()
+        dump (w,h,map)
+
 Console.ReadLine() |> ignore
