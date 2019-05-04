4d4b739ee24192d14b07f09de0c938f8858eb317
Andri <andriniaina@gmail.com>
2019-05-04 21:40:47 +0200

    IO monad

--- codinggame/Program.fs
+++ codinggame/Program.fs
@@ -2,26 +2,19 @@
 (* the standard input according to the problem statement. *)
 open System
 
-open Winamax
+type IOSideEffect<'a> = IO of 'a
 
+type IOBuilder() =
+    member x.Bind(expr, func) =
+        match expr with
+        | IO(v) -> (func v)
 
-let w,h,map = 3,3,[Ball(2);Grass;Water;Water;Grass;Hole(false);Grass;Hole(false);Ball(1)]
+    member x.Return v = IO(v)
 
-(*
-
-let d = findPossibleLandingSpot 0 (3,3,map) |> List.ofSeq
-assert (d.Length=2)
-let newMaps = shootEverywhere 0 2 (w,h,map) |> List.ofSeq
-
-for map in newMaps do
-    stdout.WriteLine (dump (w,h,map))
-*)
-
-let solutions = findSolutions (w,h,map) |> List.ofSeq
-for map in solutions do
-    let isAllHolesFilled = true //map |> List.filter (isHole) |> List.forall (isFilledHole)
-    if isAllHolesFilled then
-        stdout.WriteLine()
-        dump (w,h,map)
-
-Console.ReadLine() |> ignore
+let ConsoleRead() = IO(stdin.ReadLine())
+let iobuilder = IOBuilder()
+let readConsole f = iobuilder {
+    let! data = ConsoleRead()
+    return f data
+}
+let input = readConsole int
