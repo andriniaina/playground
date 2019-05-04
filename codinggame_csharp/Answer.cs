affb23695054ec03cb6b51c434d7753ba0d23e93
Andri <andriniaina@gmail.com>
2019-05-04 23:55:50 +0200

    HeapSort

--- codinggame_csharp/Answer.cs
+++ codinggame_csharp/Answer.cs
@@ -11,34 +11,128 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
-
-class Solution {
-
-    // Complete the twoStrings function below.
-    static string twoStrings(string s1, string s2) {
-s1 = new String(s1.Distinct().ToArray());
-s2 = new String(s2.Distinct().ToArray());
-
-return s1.Any( c => s2.IndexOf(c)>=0) ? "YES" : "NO";
-
+class SortAlgorithms
+{
+    static void BubbleSort<T>(T[] a) where T : IComparable
+    {
+        var hasSwapped = false;
+        do
+        {
+            hasSwapped = false;
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                if (a[i].CompareTo(a[i + 1]) > 0)
+                {
+                    var tmp = a[i + 1];
+                    a[i + 1] = a[i];
+                    a[i] = tmp;
+                    hasSwapped = true;
+                }
+            }
+        } while (hasSwapped);
     }
 
-    static void Main(string[] args) {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int q = Convert.ToInt32(Console.ReadLine());
-
-        for (int qItr = 0; qItr < q; qItr++) {
-            string s1 = Console.ReadLine();
-
-            string s2 = Console.ReadLine();
-
-            string result = twoStrings(s1, s2);
-
-            textWriter.WriteLine(result);
+    static void HeapSort<T>(T[] a) where T : IComparable
+    {
+        void swapIfNecessary2(ref T v1, ref T v2)
+        {
+            if (v2.CompareTo(v1) > 0)
+            {
+                var tmp = v2; v2 = v1; v1 = tmp;
+            }
+        }
+        void swapIfNecessary3(ref T v1, ref T v2, ref T v3)
+        {
+            swapIfNecessary2(ref v1, ref v2);
+            swapIfNecessary2(ref v1, ref v3);
+        }
+        int getParent(int pi)
+        {
+            return pi % 2 == 0 ? pi / 2 - 1 : pi / 2;
+        }
+        (int, int) getChildren(int i)
+        {
+            var c1 = 2 * i + 1;
+            var c2 = 2 * i + 2;
+            return (c1, c2);
+        }
+        // heapify
+        for (int i = 0; i < (a.Length); i++)
+        {
+            var (c1, c2) = getChildren(i);
+            if (c2 < a.Length)
+                swapIfNecessary3(ref a[i], ref a[c1], ref a[c2]);
+            else if (c1 < a.Length)
+                swapIfNecessary2(ref a[i], ref a[c1]);
+            stdout(join(a));
+            // bubble up the parent i node if necessary
+            var pi = i;
+            while (pi > 0)
+            {
+                var parent = getParent(pi);
+                swapIfNecessary2(ref a[parent], ref a[pi]);
+                stdout(join(a));
+                pi = parent;
+            }
         }
 
-        textWriter.Flush();
-        textWriter.Close();
+        // sort: extract roots
+        for (int i = 0; i < a.Length; i++)
+        {
+            var rightBoundary = a.Length - 1 - i;
+            // extract root
+            swapIfNecessary2(ref a[rightBoundary], ref a[0]);
+            stdout(join(a));
+
+            // fix the tree: bubble down the wrong root
+            var c = 0;
+            while (2 * c + 1 < rightBoundary)
+            {
+                var (c1, c2) = getChildren(c);
+                if (c2 >= a.Length || a[c1].CompareTo(a[c2]) > 0)
+                {
+                    swapIfNecessary2(ref a[c], ref a[c1]);
+                    stdout(join(a));
+                    c = c1;
+                }
+                else// 
+                {
+                    if (c2 < rightBoundary)
+                    {
+                        swapIfNecessary2(ref a[c], ref a[c2]);
+                        stdout(join(a));
+                    }
+                    c = c2;
+                }
+            }
+        }
+    }
+
+    /*
+67
+34                   6
+8          9         5       6
+7    7     8    8    4    4  5 5
+1 3  3 3   4 1  4 2  2 2  3
+     */
+
+
+    static void Main(string[] args)
+    {
+        var array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 3, 4, 67, 8, 34, 2, 2, 4, 5 };
+        stdout(join(array));
+        SortAlgorithms.HeapSort(array);
+        stdout(join(array));
+    }
+
+    [System.Diagnostics.Conditional("DEBUG")]
+    private static void stdout(string v)
+    {
+        Console.WriteLine(v);
+    }
+
+    private static string join<T>(IEnumerable<T> array)
+    {
+        return string.Join(",", array);
     }
 }
