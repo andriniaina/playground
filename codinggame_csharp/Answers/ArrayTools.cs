ff1d332e91ee2a4b82159c1a89b1f0952f730627
Andri Rakotomalala <andriniaina@gmail.com>
2019-05-05 12:41:53 +0200

    unit tests

--- codinggame_csharp/Answers/ArrayTools.cs
+++ codinggame_csharp/Answers/ArrayTools.cs
@@ -7,8 +7,70 @@ public class ArrayTools
     {
         return string.Join(",", array);
     }
-    int FindLowerBoundBinarySearch(int[] a, int lowerBound)
+    /// <exception>Can return an index bigger than a.Length if no element found</exception>
+    public static int FindIndexLesserOrEqualThan(int[] a, int upperBound)
     {
-return 0;
+        var left = 0;
+        var right = a.Length - 1;
+        var i = a.Length / 2;
+        while (true)
+        {
+            if (a[i] > upperBound) // must go left
+            {
+                right = i;
+            }
+            else if (a[i] < upperBound) // must go right
+            {
+                left = i;
+            }
+            else // it's right there!
+            {
+                return i;
+            }
+            i = (left + right) / 2;
+
+            if (left + 1 >= right)
+            {
+                if (a[left] > upperBound)
+                    return -1;
+                else if (a[right] <= upperBound)
+                    return right;
+                else
+                    return left;
+            }
+        }
+    }
+    /// <exception>Can return an index bigger than a.Length if no element found</exception>
+    public static int FindIndexGreaterOrEqualThan(int[] a, int lowerBound)
+    {
+        var left = 0;
+        var right = a.Length - 1;
+        var i = a.Length / 2;
+        while (true)
+        {
+            if (a[i] > lowerBound) // must go left
+            {
+                right = i;
+            }
+            else if (a[i] < lowerBound) // must go right
+            {
+                left = i;
+            }
+            else // it's right there!
+            {
+                return i;
+            }
+            i = (left + right) / 2;
+
+            if (left + 1 >= right)
+            {
+                if (a[right] < lowerBound)
+                    return -1;
+                if (a[left] >= lowerBound)
+                    return left;
+                else //if (a[left]>=lowerBound)
+                    return right;
+            }
+        }
     }
 }
--- codinggame_csharp/Tests/UnitTest1.cs
+++ codinggame_csharp/Tests/UnitTest1.cs
@@ -3,13 +3,63 @@ using Xunit;
 
 namespace Tests
 {
-    public class UnitTest1
+    public class ArrayToolsTests
     {
         [Fact]
-        public void Test1()
+        public void Join()
         {
-            var s = ArrayTools.join(new int[] { 1, 2, 3, 4, 5, 6 });
-            Assert.Equal("1,2,3,4,5,6", s);
+            var s = ArrayTools.join(new int[] { -1, 2, 3, 4, 5, 6 });
+            Assert.Equal("-1,2,3,4,5,6", s);
+        }
+        [Fact(Timeout = 100)]
+        public void FindLowerBound_middle()
+        {
+            var s = ArrayTools.FindIndexGreaterOrEqualThan(new int[] { -1, 2, 3, 4, 5, 6 }, 2);
+            Assert.Equal(1, s);
+        }
+        [Fact(Timeout = 100)]
+        public void FindUpperBound_notinarray()
+        {
+            var s = ArrayTools.FindIndexLesserOrEqualThan(new int[] { -1, 2, 4, 5, 6 }, 3);
+            Assert.Equal(1, s);
+        }
+        [Fact(Timeout = 100)]
+        public void FindLowerBound_notinarray()
+        {
+            var s = ArrayTools.FindIndexGreaterOrEqualThan(new int[] { -1, 2, 4, 5, 6 }, 3);
+            Assert.Equal(2, s);
+        }
+        [Fact(Timeout = 100)]
+        public void FindUpperBound_middle()
+        {
+            var s = ArrayTools.FindIndexLesserOrEqualThan(new int[] { -1, 2, 3, 4, 5, 6 }, 2);
+            Assert.Equal(1, s);
+        }
+        [Fact(Timeout = 100)]
+        public void FindLowerBound_allabove()
+        {
+            var s = ArrayTools.FindIndexGreaterOrEqualThan(new int[] { 1, 2, 3, 4, 5, 6 }, 0);
+            Assert.Equal(0, s);
+            s = ArrayTools.FindIndexGreaterOrEqualThan(new int[] { 1, 2, 3, 4, 5, 6 }, -1);
+            Assert.Equal(0, s);
+        }
+        [Fact(Timeout = 100)]
+        public void FindLowerBound_allbelow()
+        {
+            var s = ArrayTools.FindIndexGreaterOrEqualThan(new int[] { 1, 2, 3, 4, 5, 6 }, 100);
+            Assert.Equal(-1, s);
+        }
+        [Fact(Timeout = 100)]
+        public void FindUpperBound_allbelow()
+        {
+            var s = ArrayTools.FindIndexLesserOrEqualThan(new int[] { 0, 1, 2, 3, 4, 5 }, 5);
+            Assert.Equal(5, s);
+            s = ArrayTools.FindIndexLesserOrEqualThan(new int[] { 0, 1, 2, 3, 4, 5 }, 100);
+            Assert.Equal(5, s);
+            s = ArrayTools.FindIndexLesserOrEqualThan(new int[] { 0, 1, 2, 3, 4, 5 }, -1);
+            Assert.Equal(-1, s);
+            s = ArrayTools.FindIndexLesserOrEqualThan(new int[] { -100, 0, 1, 2, 3, 4, 5 }, -10000);
+            Assert.Equal(-1, s);
         }
     }
 }
